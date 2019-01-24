6e72bb38449f5081dd37cab18264c6cf6ba70dc3
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-01-24 00:24:00 -0800

    Material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ParticipantSearchComponent } from './components/participant-search/participant-search.component';
@@ -16,7 +18,12 @@ import { UploadComponent } from './components/upload/upload.component';
     ParticipantInformationComponent,
     UploadComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
